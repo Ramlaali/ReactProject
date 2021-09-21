17806fe088ca3c12367d3a8633b2586619e042d2
Ramla Abdullahi <ramlaali.abd@gmail.com>
2021-09-21 19:22:38 -0400

    Dropdown prints out movie names

--- src/components/MoviesReview.js
+++ src/components/MoviesReview.js
@@ -12,14 +12,16 @@ function MoviesReview(){
 
     let handleMovieChange = (e) => {
         setMovieName(e.target.value)
+        console.log(e.target.value)
     }
    
     return (
         <div>
             <h1>Movie Review Page</h1>
 
-            <select name= "movieNames" onChange= {handleMovieChange}>
-                <option value="Select a Movie" > {db.movies.map((movie) => <option value={movie.moviename}> {movie} </option>)} </option>
+            <select onChange= {handleMovieChange}>
+                <option value="Select a Movie" > </option>
+                {db.movies.map((movie) => ( <option value={movie.moviename} key= {movie.key}> {movie.moviename} </option>))} 
             </select>
             <WriteReview/>
         </div>
